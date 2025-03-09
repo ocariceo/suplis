2d6ab088916d153c33f08231348e68456b9c1789
ocariceo <ocariceor@yahoo.es>
2025-03-09 20:08:03 -0300

    Update settings.py

--- compras/settings.py
+++ compras/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["suplis.onrender.com", "localhost"]
 
 
 # Application definition
